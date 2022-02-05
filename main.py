179917b8699b3c07677a7d9725b02b5f58f655d8
Julian Canepa <jjlcanepa@gmail.com>
2022-02-05 12:34:41 -0800

    Update method name, add block header.

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-def longest_common_subsequence_recursive(A:list, B:list) -> int:
+'''
+Find the length of the longest common subsequence
+using a recursive approach.
+'''
+def lcs_recursive(A:list, B:list) -> int:
     # one (or more) lists are empty
     if not A or not B:
         return 0
     # matching character found, increment count
     if A[0] == B[0]:
-        return 1 + longest_common_subsequence_recursive(A[1:], B[1:])
-
+        return 1 + lcs_recursive(A[1:], B[1:])
     # return largest count resulting from either subproblems
     return max(
-        longest_common_subsequence_recursive(A[1:], B),
-        longest_common_subsequence_recursive(A, B[1:]))
+        lcs_recursive(A[1:], B),
+        lcs_recursive(A, B[1:]))
 
 
 if __name__ == '__main__':
     A = ['b', 'd']
     B = ['a', 'b', 'c', 'd']
     print(
-        longest_common_subsequence_recursive(A, B))
+        lcs_recursive(A, B))
